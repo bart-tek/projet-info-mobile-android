f22db3678d5d1c1ab1e59e7c9e996862fbc0ec71
bart-tek <barthelemy.tek@imt-atlantique.net>
2020-11-13 17:25:22 +0100

    appbar

--- app/src/main/java/com/example/henripotier/view/LibraryActivity.kt
+++ app/src/main/java/com/example/henripotier/view/LibraryActivity.kt
@@ -1,6 +1,7 @@
 package com.example.henripotier.view
 
 import android.os.Bundle
+import android.view.Menu
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.henripotier.R
@@ -21,7 +22,12 @@ class LibraryActivity : AppCompatActivity(), LibraryContractInterface.View {
     }
 
     override fun initView() {
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_library, menu)
+        return true
     }
 
     override fun updateViewData() {
